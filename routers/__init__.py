40c645e468eb98e01ec62ed98921716054c0d9ad
eliblurr <elvissegbawu@gmail.com>
2021-09-29 13:56:49 +0000

    Initialise tenant tables on tenant creation

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,7 +4,7 @@ import routers.priority.models
 import routers.tenant.models
 
 '''
-    Do this to order the the way each model interacts with Base from database
+    Do this to order the the way each model interacts with Base from database.py
     Tenant Model is called last so Base will have all metadata from all the 
     other models[for use in create_tenant_schema]
 '''
