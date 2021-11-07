e1bca07e3cdf49ff9e6dfc6df844f0bbb0a49b2e
eliblurr <elvissegbawu@gmail.com>
2021-11-07 14:19:07 +0000

    Combine Manufacturer with different bases:Read

--- routers/manufacturer/crud.py
+++ routers/manufacturer/crud.py
@@ -1,5 +1,5 @@
 from . import models, schemas
 from cls import CRUD
-
-
-manufacturer = lambda tenant=True : CRUD(models.Manufacturer2, extra_models=[models.Manufacturer]) if tenant else CRUD(models.Manufacturer, extra_models=[models.Manufacturer2])
+ 
+manufacturer = lambda tenant=True : CRUD(models.Manufacturer2, extra_models=[models.Manufacturer])  \
+    if tenant else CRUD(models.Manufacturer, extra_models=[models.Manufacturer2])
