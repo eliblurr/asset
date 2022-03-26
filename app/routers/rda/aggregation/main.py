8bff833d9c266affcb326c7ab4a7bb78d92a5de5
eliblurr <elvissegbawu@gmail.com>
2022-03-26 21:06:31 +0000

    name EP

--- app/routers/rda/aggregation/main.py
+++ app/routers/rda/aggregation/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/')
-async def read():
+@router.post('/', name='aggregate')
+async def aggregate():
     pass
 
--- app/routers/rda/report/main.py
+++ app/routers/rda/report/main.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/')
-async def read():
+@router.post('/', name='generate report')
+async def generate_report():
     pass
