0a24711c0a396480bcd353b565f771970a023c8e
eliblurr <elvissegbawu@gmail.com>
2022-05-04 20:37:46 +0000

    verify source

--- app/routers/rda/_/models.py
+++
@@ -1,8 +0,0 @@
-from routers.department.models import Department
-from routers.inventory.models import Inventory
-from routers.proposal.models import Proposal
-from routers.request.models import Request
-from routers.policy.models import Policy # to br removed
-from routers.branch.models import Branch
-from routers.tenant.models import Tenant
-from routers.asset.models import Asset
