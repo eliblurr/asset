893183c98de3f32b71c634ebc275c85a6d98a8b2
eliblurr <elvissegbawu@gmail.com>
2022-05-08 14:20:14 +0000

    add auth header to endpoints

--- app/routers/faqs/main.py
+++ app/routers/faqs/main.py
@@ -9,7 +9,7 @@ from utils import r_fields
 router = APIRouter()
 
 @router.post('/', response_model=schemas.FAQ, status_code=201, name='FAQ') # is authenticated, is system admin
-async def create(payload:schemas.CreateFAQ, db:Session=Depends(get_db)):
+async def create(payload:schemas.CreateFAQ, db:Session=Depends(get_db)): # , is_validated=Depends(validate_bearer)
     return await crud.faq.create(payload, db)
 
 @router.get('/', response_model=schemas.FAQList, name='FAQ')
@@ -22,9 +22,9 @@ async def read_by_id(id:int, fields:List[str]=r_fields(crud.faq.model), db:Sessi
     return await crud.faq.read_by_id(id, db, fields)
 
 @router.patch('/{id}', response_model=schemas.FAQ, name='FAQ') # is authenticated, is system admin
-async def update(id:int, payload:schemas.UpdateFAQ, db:Session=Depends(get_db)):
+async def update(id:int, payload:schemas.UpdateFAQ, db:Session=Depends(get_db)):# , is_validated=Depends(validate_bearer)
     return await crud.faq.update(id, payload, db)
 
 @router.delete('/{id}', name='FAQ', status_code=204) # is authenticated, is system admin
-async def delete(id:int, db:Session=Depends(get_db)):
+async def delete(id:int, db:Session=Depends(get_db)): # , is_validated=Depends(validate_bearer)
     await crud.faq.delete(id, db)
