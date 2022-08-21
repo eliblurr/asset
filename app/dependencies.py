c9afd3532d56d26435dc7847cee49bae5b976332
eliblurr <elvissegbawu@gmail.com>
2022-08-21 09:46:20 +0000

    debug

--- app/dependencies.py
+++ app/dependencies.py
@@ -44,7 +44,7 @@ async def validate_bearer(token:str=Depends(oauth2_scheme), db=Depends(get_db)):
     try:
         if await is_token_blacklisted(token, db):
             raise BlacklistedToken('token blacklisted')  
-        return decode_jwt(token)
+        # return decode_jwt(token)
     except Exception as e:
         raise HTTPException(
             status_code=401 if isinstance(e, ExpiredSignatureError) else 500, 
